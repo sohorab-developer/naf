e38311c1c83df8f1576e485c7787014d522f8c55
Sohorab Mahmud Fahim <sohorab.ndc.edu@gmail.com>
2025-04-09 10:05:15 +0600

    Delete js directory

--- js/preloader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    const preloader = document.getElementById('preloader');
-    const content = document.getElementById('content');
-    preloader.style.display = 'none';
-    content.style.display = 'block';
-});
